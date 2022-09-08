903d10c83349a69dc61e1936e7d9dde109477d63
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-09 00:09:19 +0200

    update

---
+++ client/src/PortfolioContainer/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Header(){
+    
+    return(
+        <div>
+
+        </div>
+    )
+}
