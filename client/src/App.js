7f392d375037c932fe1067fab6f14218d01bcbb6
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-08 22:09:52 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Profile from './PortfolioContainer/Home/Profile';
+import Home from './PortfolioContainer/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      <Home/>
     </div>
   );
 }
